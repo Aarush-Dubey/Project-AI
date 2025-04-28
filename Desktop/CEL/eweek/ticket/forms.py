a847a583cfb5f346343d104ca9945d108fcecf57
Aarush Dubey <f20241284@pilani.bits-pilani.ac.in>
2025-04-28 17:01:46 +0530

    Delete Desktop directory

--- Desktop/CEL/eweek/ticket/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class TicketForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
--- Desktop/CEL/eweek/ticket/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="home"),
-    path('signup/', views.signup, name="signup"),
-    path('confirmation/', views.confirmation, name="confirmation"),
-]
